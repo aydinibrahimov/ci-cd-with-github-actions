43dc4bf50f3e377023156a57db747a34a9b22019
aydinibrahimov <aydininf@gmail.com>
2024-03-14 21:38:12 +0400

    .github directory for github action

--- src/main/java/com/example/cicdpipeline/CicdpiplineApplication.java
+++ src/main/java/com/example/cicdpipeline/CicdpiplineApplication.java
@@ -1,4 +1,4 @@
-package com.example.cicdpipline;
+package com.example.cicdpipeline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/example/cicdpipeline/controller/MyController.java
+++ src/main/java/com/example/cicdpipeline/controller/MyController.java
@@ -1,4 +1,4 @@
-package com.example.cicdpipline.controller;
+package com.example.cicdpipeline.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
