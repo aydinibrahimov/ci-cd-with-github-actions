f4f33566dc2731e134a0d461c446c0ef75986d0d
aydinibrahimov <aydininf@gmail.com>
2024-03-14 21:39:32 +0400

    .github directory for github action

--- src/main/java/com/example/cicdpipeline/CicdPipelineApplication.java
+++ src/main/java/com/example/cicdpipeline/CicdPipelineApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CicdpiplineApplication {
+public class CicdPipelineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CicdpiplineApplication.class, args);
+		SpringApplication.run(CicdPipelineApplication.class, args);
 	}
 
 }
