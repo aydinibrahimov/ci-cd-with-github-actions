89c8a85f8896af52647617cec9885dfc98136831
aydinibrahimov <aydininf@gmail.com>
2024-03-14 21:07:20 +0400

    Dockerfile

---
+++ src/main/java/com/example/cicdpipline/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.example.cicdpipline.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("github-actions/cicd/v1")
+public class MyController {
+
+    @GetMapping
+    public String helloVisitor(){
+        return "Hello visitor";
+    }
+}
